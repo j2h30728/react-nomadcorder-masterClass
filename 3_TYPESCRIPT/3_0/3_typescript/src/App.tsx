28384110714dac15e5ff40a8a4c42d7abf5ca95f
LeeJiHyeon <rachel2148072@gmail.com>
2023-01-04 22:03:35 +0900

    3.7 Recap

--- 3_TYPESCRIPT/3_0/3_typescript/src/App.tsx
+++ 3_TYPESCRIPT/3_0/3_typescript/src/App.tsx
@@ -7,10 +7,26 @@ function App() {
   const H1 = styled.h1`
     color: ${props => props.theme.textColor};
   `;
-
+  interface DummyProps {
+    text: string;
+    otherThingHere?: boolean;
+  }
+  function Dummy({ text, otherThingHere }: DummyProps) {
+    return <H1>{text}</H1>;
+  }
+  const onClickForm = (event: React.FormEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.value);
+  };
+  const onClickMouse = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.value);
+  };
   return (
     <Container>
-      <H1>Protected</H1>
+      <Dummy text="Hello" />
+      <form>
+        <button onClick={onClickForm}>Click me</button>
+      </form>
+      <button onClick={onClickMouse}>Click me</button>
     </Container>
   );
 }
