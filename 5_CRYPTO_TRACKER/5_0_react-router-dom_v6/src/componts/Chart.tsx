2249cd18d80d6fdfbd6237998d6c0c9cc3c38372
LeeJiHyeon <rachel2148072@gmail.com>
2023-01-24 16:25:57 +0900

    5.13 Price Chart part Two : react-apexcharts 사용하여 그래프 생성

--- 5_CRYPTO_TRACKER/5_0_react-router-dom_v6/src/componts/Chart.tsx
+++ 5_CRYPTO_TRACKER/5_0_react-router-dom_v6/src/componts/Chart.tsx
@@ -2,14 +2,73 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "../api";
 import { useOutletContext } from "react-router-dom";
+import ApexChart from "react-apexcharts";
+
 interface ChartProps {
   coinId: string;
 }
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 export default function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  console.log(coinId);
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>data</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map(price => parseFloat(price.close)) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+            },
+            stroke: {
+              curve: "smooth",
+            },
+            title: {
+              text: coinId,
+              align: "center",
+            },
+            grid: {
+              show: false,
+            },
+            xaxis: {
+              labels: {
+                show: false,
+              },
+            },
+            yaxis: {
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
