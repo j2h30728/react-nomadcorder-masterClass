999b4ea1600b7e3db0170ba0331fc15372871e1d
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-06 00:32:05 +0900

    8.7,8.8: useMotionValue로 좌표값 확인, useTransform()로 useMotionValue를 매핑해서 다른 값 범위의 motionValue로 변환해줌

--- 8_ANIMATION/review/src/App.tsx
+++ 8_ANIMATION/review/src/App.tsx
@@ -1,33 +1,21 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
-  console.log(biggerBoxRef);
+  const x = useMotionValue(0);
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin //드래그 이후 다시 원점으로 되돌아옴
-          dragConstraints={biggerBoxRef} //드래그 제약조건
-          dragElastic={0.5} // dragConstraints 영역을 벗어나면 생기는 탄성
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
 
 export default App;
-
-const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
-};
 
 const Box = styled(motion.div)`
   width: 200px;
@@ -36,15 +24,7 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
