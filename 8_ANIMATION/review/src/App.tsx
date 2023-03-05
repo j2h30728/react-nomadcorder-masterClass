9e8ce3ef1384a8fef05d954801fc40b2179d5860
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-06 00:50:34 +0900

    8.9:useScroll() 스크롤에 따라 motionValue가 변경되고 useTransform을 이용해 value에 따라  애니메이션 다르게 적용가능

--- 8_ANIMATION/review/src/App.tsx
+++ 8_ANIMATION/review/src/App.tsx
@@ -1,16 +1,28 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 function App() {
   const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+
+  console.log(scrollYProgress);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
   useEffect(() => {
-    x.onChange(() => console.log(x.get()));
-  }, [x]);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+    scrollYProgress.onChange(() => console.log(scrollYProgress.get()));
+  }, [scrollYProgress]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
@@ -25,8 +37,8 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
